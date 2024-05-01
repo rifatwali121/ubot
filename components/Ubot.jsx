a445db277177f67554b2bb2fb76e734cef78528c
Waliullah Rifat <143581537+rifatwali121@users.noreply.github.com>
2024-05-01 22:11:11 +0600

    add bot design

--- components/Ubot.jsx
+++ components/Ubot.jsx
@@ -137,3 +137,233 @@ export default function Home() {
   if (!browserSupportsSpeechRecognition) {
     return <h1>Browser does support speech recognition.</h1>;
   }
+ return (
+    <>
+      <BotWrapper>
+        <Header onClick={() => setBotOpen(botOpen == false ? true : false)}>
+          <p>
+            <strong>Got a question?</strong>
+          </p>
+          <CloseBtn>
+            <IoClose />
+          </CloseBtn>
+        </Header>
+        <MessageWindow className={botOpen == false ? "d-none" : "d-block"}>
+          <ul className="chatbot__messages">
+            <Alert show={showIntro} variant="success">
+              <div className="d-flex align-items-center">
+                <Image
+                  src={botLogo}
+                  height={44}
+                  width={44}
+                  alt="bot-logo"
+                ></Image>
+                <Alert.Heading className="ms-2 mt-2">
+                  Hello I&apos;m Ubot.
+                </Alert.Heading>
+              </div>
+              <p>
+                I am an artificial intelligence-powered virtual assistant. I can
+                find university-related information, provide important links,
+                help you apply, pay, and more. How can I help you today? Click
+                the button below to wake me up!
+              </p>
+              <hr />
+              <div className="d-flex justify-content-end">
+                <Button
+                  onClick={() => {
+                    setShowIntro(false);
+                    setInputOpen(true);
+                  }}
+                  variant="outline-success"
+                >
+                  Get Started
+                </Button>
+              </div>
+            </Alert>
+
+            {/* {userData.map((item, index) => {
+							return (
+								<>
+									<IsUser message={item.userInput} />
+
+									<BotTyping />
+
+									<IsBot message={()} />
+								</>
+							);
+						})} */}
+            {/* <BotTyping />
+						<IsBot message={'uuu'} />
+						<IsUser message={'BBB'} /> */}
+
+            {/* .......combiners */}
+            {userArr.map((item, index) => {
+              return (
+                <>
+                  <IsUser message={item.userInput} />
+
+                  {/* {userArr.length - 1 === index ? <BotTyping /> : null} */}
+
+                  <IsBot message={botArr[index].fetchData} />
+                </>
+              );
+            })}
+
+            {/* {botArr.map((item, index) => {
+							return (
+								<>
+									<IsBot message={item.fetchData} />
+								</>
+							);
+						})} */}
+
+            {/* .......combiners */}
+
+            {/* {userArr.map((item, index) => {
+							return (
+								<>
+									<IsUser message={item.userInput} />
+								</>
+							);
+						})} */}
+
+            {/* {botArr.map((item, index) => {
+							return (
+								<>
+									<IsBot message={item.fetchData} />
+								</>
+							);
+						})} */}
+          </ul>
+          <div ref={messagesEndRef} />
+        </MessageWindow>
+
+        <form
+          action=""
+          onSubmit={handleSubmit}
+          className={botOpen == false ? "d-none" : "d-block"}
+        >
+          <div
+            className={
+              inputOpen == false ? "chatbot__entry  d-none" : "chatbot__entry"
+            }
+          >
+            <input
+              onChange={(e) => setUserInput(e.target.value)}
+              value={userInput}
+              name="userInput"
+              type="text"
+              autoComplete="off"
+              placeholder="Write a message..."
+              className="chatbot__input"
+            />
+            <MicIcon onClick={recStart}>
+              <IoMic className={listening ? "text-danger" : ""} />
+            </MicIcon>
+            <RedDot>
+              <IoEllipse className={listening ? "visible" : "invisible"} />
+            </RedDot>
+            <SendBtn
+              type="submit"
+              className={
+                userInput.replace(/\s{2,}/g, " ").trim() === ""
+                  ? "text-dark"
+                  : "text-primary"
+              }
+            >
+              <IoSend />
+            </SendBtn>
+          </div>
+        </form>
+      </BotWrapper>
+    </>
+  );
+}
+const BotWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+  border-radius: 5px;
+  box-shadow: 0 -6px 99px -17px rgba(0, 0, 0, 0.68);
+  z-index: 99;
+
+  @media screen and (min-width: 640px) {
+    max-width: 420px;
+    right: 80px;
+    top: auto;
+  }
+`;
+const Header = styled.div`
+  color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #275cab;
+  height: 54px;
+  padding: 0 20px;
+  width: 100%;
+  cursor: pointer;
+  border-radius: 5px 5px 0px 0px;
+  transition: background-color 0.2s ease;
+  border-bottom: 3px solid #94caf1;
+  p {
+    margin: 0;
+  }
+  &:hover {
+    background-color: #0f3c7e;
+  }
+`;
+const CloseBtn = styled.div`
+  svg {
+    font-size: 1.3rem;
+  }
+`;
+const MessageWindow = styled.div`
+  height: calc(100% - (54px + 60px));
+  padding: 40px 20px 20px;
+  background-color: #fff;
+  overflow-x: none;
+  overflow-y: auto;
+  &::-webkit-scrollbar {
+    width: 0px;
+    background: transparent;
+  }
+
+  @media screen and (min-width: 640px) {
+    height: 380px;
+  }
+`;
+const SendBtn = styled.button`
+  border: none;
+  cursor: pointer;
+  margin: 0;
+  padding: 0;
+  background: none;
+  svg {
+    font-size: 1.2rem;
+  }
+`;
+const MicIcon = styled.div`
+  cursor: pointer;
+  svg {
+    font-size: 1.3rem;
+  }
+`;
+const RedDot = styled.div`
+  margin-right: 5px;
+  svg {
+    font-size: 0.7rem;
+    color: #bb2d3b8c;
+    animation: blinker 1.5s cubic-bezier(0.5, 0, 1, 1) infinite alternate;
+    @keyframes blinker {
+      from {
+        opacity: 1;
+      }
+      to {
+        opacity: 0;
+      }
+    }
+  }
+`;
