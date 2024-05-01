4dc94cd547106856bc9b30af3b40f390e4f84913
Rimjim-Bipa <167183103+Rimjim-Bipa@users.noreply.github.com>
2024-05-01 21:57:33 +0600

    add Footer.jsx

---
+++ components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+// import './footer.css';
+
+const Footer = () => <footer className="page-footer font-small blue pt-4">
+    <div className="Footer">
+                <div className="container">
+                    <div className="row">
+                        <div className="">
+                            <h3 className="text-primary"><span>Message From Head of The Department</span></h3>
+                            <p>Welcome to the Department of Computer Science and Engineering (CSE) at University of Asia Pacific (UAP), Bangladesh. Computer Science is an exciting, challenging and dynamic discipline which is now widely recognized as an essential source of tools and techniques for advancements in nearly all spheres of human endeavor.</p>
+                            <br />
+                            <p>
+                            The Department of CSE started its journey in 1996 by offering B.Sc. Engg. in CSE and later on in 2006 added offering graduate program. Currently, the Department has a total of 23 skilled full-time faculty members. Among them, 7 faculty members have Ph.D. degree from different reputed universities. The Department facilities include state-of-the-art classrooms and lab facilities. We have five outstanding computer programming labs with latest configuration computers and equipment, one dedicated lab for final year student’s project or thesis work, one digital lab, and one industry-standard gaming lab established with the support from ICT Division of Bangladesh Government.
+                            </p>
+                            
+                       
+                    </div>
+                    </div>
+                </div>
+            </div>
+            <div className='Last-footer text-center text-primary '>
+                <p>Design By Waliullah Jahid Hasan Jami Sakib Chowdhary Rimjhim Bipa Nayem Fahim Montasir</p>
+            </div>
+</footer>
+
+export default Footer
