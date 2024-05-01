f7f99651b9a9df4249135a900d593066705016b8
Waliullah Rifat <143581537+rifatwali121@users.noreply.github.com>
2024-05-01 21:36:58 +0600

    Create index.js

---
+++ components/index.js
@@ -0,0 +1,7 @@
+export { default as BotTyping } from './BotTyping';
+export { default as Feature } from './Feature';
+export { default as HeroSlider } from './HeroSlider';
+export { default as IsBot } from './IsBot';
+export { default as IsUser } from './IsUser';
+export { default as Nav } from './Nav';
+export { default as Ubot } from './Ubot';
